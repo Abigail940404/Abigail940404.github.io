dd3213daccae41b9cdb0c2be9ffb6f336f24407f
Abigail940404 <13752544732@163.com>
2018-10-15 13:38:35 +0800

    Site updated: 2018-10-15 13:38:35

---
+++ js/src/custom.js
@@ -0,0 +1,8 @@
+/* 轮播背景图片 */
+$(function () {
+    $.backstretch([  
+          "http://h.hiphotos.baidu.com/zhidao/pic/item/3b87e950352ac65cf7942699fff2b21192138a43.jpg",
+          "http://b-ssl.duitang.com/uploads/item/201709/23/20170923214959_GCr5E.jpeg",
+          "http://img17.3lian.com/d/file/201702/20/640279eb993ee6058871f064b8c93096.jpg"
+    ], { duration: 50000, fade: 1500 });  
+});
